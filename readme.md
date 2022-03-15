2debf31395ae9d7d772ae843b3fbeab27e911ece
Rachhen <rachhen.it@outlook.com>
2022-03-15 14:45:27 +0700

    added readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+## Cambodia Gazetteer
+
+[See NCDD](http://db.ncdd.gov.kh/gazetteer/view/index.castle)
+
+### Run web crawler
+
+```sh
+npm install
+npx ts-node crawler.ts
+```
+
+### Run web server serve for crawler
+
+```sh
+npm install
+npx ts-node server.crawler.ts
+```
---
+++ server.crawler.ts
@@ -0,0 +1,12 @@
+import { join } from "path";
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router(join(__dirname, "crawler.json"));
+const middlewares = jsonServer.defaults({ readOnly: true });
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
